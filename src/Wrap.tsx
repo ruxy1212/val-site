1fb2ef69edd609b9b203fc5163aea16994b68fc9
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2026-02-14 20:21:57 +0100

    Add z-index to GithubDogEar component

--- src/Wrap.tsx
+++ src/Wrap.tsx
@@ -7,7 +7,7 @@ const GithubDogEar = () => {
  // const foldSize = 100;
   return (
     <>
-     <div className="absolute bottom-0 right-0 w-24 md:w-36">
+     <div className="absolute bottom-0 right-0 z-50 w-24 md:w-36">
       <div className="relative w-full">
        <img src={dogEar} className="w-full" />
        <button className="absolute top-2 right-2">
