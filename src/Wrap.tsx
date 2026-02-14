5ddd023f1afe1d4a86f16a9d3fe91088f8e79d37
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2026-02-14 20:57:05 +0100

    Update Wrap.tsx

--- src/Wrap.tsx
+++ src/Wrap.tsx
@@ -7,10 +7,10 @@ const GithubDogEar = () => {
  // const foldSize = 100;
   return (
     <>
-     <div className="absolute bottom-0 right-0 z-50 w-24 md:w-36">
+     <div className="fixed bottom-0 right-0 z-50 w-24 md:w-36">
       <div className="relative w-full">
        <img src={dogEar} className="w-full" />
-       <a href="https://github.com/ruxy1212/val-site/fork" target="_blank" className="absolute bottom-2 right-2">
+       <a href="https://github.com/ruxy1212/val-site/fork" target="_blank" className="absolute bottom-2 right-2 p-1.5 rounded-full text-white bg-black hover:bg-gray-800 transition-colors">
         <Github size={24} />
        </a>
       </div>
