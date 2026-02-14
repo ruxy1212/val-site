fb079b58108a371b692954602ea1351237e8f29d
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2026-02-14 20:18:32 +0100

    Update Wrap component to use dog-ear SVG
    Replaced custom CSS import with dog-ear SVG image and updated button structure.

--- src/Wrap.tsx
+++ src/Wrap.tsx
@@ -1,18 +1,20 @@
 import { Github } from 'lucide-react';
 import './index.css'
-import './custom.css'
+import dogEar from './assets/dog-ear.svg'
 import App from './App.tsx'
 
 const GithubDogEar = () => {
  // const foldSize = 100;
   return (
     <>
-     <button className="dogear-button">
+     <div className="absolute bottom-0 right-0 w-24 md:w-36">
+      <div className="relative w-full">
+       <img src={dogEar} className="w-full" />
+       <button className="absolute top-2 right-2">
         <Github size={20} />
-      </button>
-
-      {/* Folded Corner */}
-      <div className="dogear-fold" />
+       </button>
+      </div>
+     </div>
         <App />
     </>
   );
